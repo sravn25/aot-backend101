5b3ee56502802e163871f77b6103d61e57a2764d
sravn25 <victor.sravn24@gmail.com>
2026-05-18 02:13:31 +0800

    feat: basic router setup

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+const taskRouter = require("./routes/tasks");
+app.use("/tasks", taskRouter);
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
